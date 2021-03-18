8daa4d0b9e2443ac125029153bab3bd47e847243
Elwin <elwin.carlos09@gmail.com>
2021-03-18 13:05:03 +0000

    add some failing tests :(

--- test/index.test.js
+++ test/index.test.js
@@ -1,23 +1,25 @@
 //Import js file
 const index = require('../script/index')
 
+//Set up mock library
+jest.mock(index);
+
+
 //Set up overall test suite for index.js functions
-
-    //Need to mock functions?
-
 describe ('index.js', () => {
 
     //Template for sub test suites
-    describe('...', () => {
-        beforeEach(() => {
-            document.documentElement.innerHTML = '<element id="...">...</element>';
-            global.id = document.querySelector('#id');
-        })
+    describe('beginSearch', () => {
+        // beforeEach(() => {
+        //     document.documentElement.innerHTML = '<element id="search-btn">...</element>';
+        //     global.id = document.querySelector('');
+        // })
 
-        test('test for ...', () => {
-            //Edit as required
-            index.function();
-            expect(id.querySelectorAll('child-element')).toBe('desired result!');
+        test('test that beginSearch contains a callback function', () => {
+            const fakeClear = jest.fn();
+            index.beginSearch(e);
+            expect(fakeClear).toHaveBeenCalledTimes(1);
+            // expect(id.querySelectorAll('child-element')).toBe('desired result!');
         })
     })
 
